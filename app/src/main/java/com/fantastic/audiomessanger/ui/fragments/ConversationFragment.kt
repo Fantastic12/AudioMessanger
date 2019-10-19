890e1b0ba3359579c2bcc2f68faa4680f1337bf3
Nazar <haker658@gmail.com>
2019-10-20 00:58:23 +0300

    Refactored code

--- app/src/main/java/com/fantastic/audiomessanger/ui/fragments/ConversationFragment.kt
+++ app/src/main/java/com/fantastic/audiomessanger/ui/fragments/ConversationFragment.kt
@@ -1,7 +1,6 @@
 package com.fantastic.audiomessanger.ui.fragments
 
 import android.Manifest
-import android.app.Activity
 import android.app.Application
 import android.arch.lifecycle.Observer
 import android.arch.lifecycle.ViewModelProviders
@@ -16,112 +15,97 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import com.fantastic.audiomessanger.R
-import com.fantastic.audiomessanger.interfaces.ListenerFragment
 import com.fantastic.audiomessanger.ui.adapters.AudioListAdapter
 import com.fantastic.audiomessanger.ui.adapters.PersonListAdapter
 import com.fantastic.audiomessanger.utils.InjectorUtils
 import com.fantastic.audiomessanger.viewModel.ConversationViewModel
 import kotlinx.android.synthetic.main.conversation_fragment.*
-import kotlinx.android.synthetic.main.conversation_fragment.view.*
-
-class ConversationFragment : Fragment(), ListenerFragment{
 
 class ConversationFragment : Fragment() {
 
-    private var isClickedRecordAudio = false
+        private var isClickedRecordAudio = false
 
-    private var adapter = AudioListAdapter(emptyList())
-    private var adapterPerson = PersonListAdapter(Application(), emptyList())
+        private var adapter = AudioListAdapter(emptyList())
+        private var adapterPerson = PersonListAdapter(Application(), emptyList())
 
-    private lateinit var viewModel: ConversationViewModel
+        private lateinit var viewModel: ConversationViewModel
 
-    companion object {
-        fun newInstance() = ConversationFragment()
-    }
-
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?)
-            : View? {
-
-        val factory = InjectorUtils.provideQuotesViewModelFactory(context!!.applicationContext)
-
-        viewModel = ViewModelProviders.of(this, factory)
-            .get(ConversationViewModel::class.java)
-
-        val view : View = inflater.inflate(R.layout.conversation_fragment, container, false)
-
-        initRecycleView(view)
-        onListener(view)
-
-        viewModel.getAllAudioMessages().observe(this, Observer {
-                audioMessages -> adapter!!.setAudioMessages(audioMessages!!)
-        })
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        initRecycleView()
-        initListeners()
-        initObservers()
-    }
-
-        viewModel.getAllPersons().observe(this, Observer {
-            persons -> adapterPerson!!.setPersons(persons!!)
-        })
-
-//        viewModel.allAudioMessages.observe(this,
-//            Observer<List<AudioMessage>> { t -> adapter!!.setAudioMessages(t!!) })
-//
-//        viewModel.allPersons.observe(this,
-//            Observer<List<Person>> { t -> adapterPerson!!.setPersons(t!!) })
-    private fun initObservers() {
-        viewModel.allAudioMessages.observe(viewLifecycleOwner,
-            Observer { t -> t?.let { adapter.setAudioMessages(it) } })
-
-        viewModel.allPersons.observe(viewLifecycleOwner,
-            Observer { t -> t?.let { adapterPerson.setPersons(it) } })
-    }
-
-    private fun initListeners() {
-        recordAudioButton.setOnClickListener {
-            if (ContextCompat.checkSelfPermission(
-                    requireActivity(),
-                    Manifest.permission.RECORD_AUDIO
-                ) != PackageManager.PERMISSION_GRANTED && ContextCompat.checkSelfPermission(
-                    requireActivity(),
-                    Manifest.permission.WRITE_EXTERNAL_STORAGE
-                ) != PackageManager.PERMISSION_GRANTED
-            ) {
-                val permissions = arrayOf(
-                    Manifest.permission.RECORD_AUDIO,
-                    Manifest.permission.WRITE_EXTERNAL_STORAGE,
-                    Manifest.permission.READ_EXTERNAL_STORAGE
-                )
-                ActivityCompat.requestPermissions(requireActivity(), permissions, 0)
-            } else {
-                viewModel.initMediaRecorder()
-                viewModel.startRecording()
-                isClickedRecordAudio = true
-                Toast.makeText(context,"start record",Toast.LENGTH_LONG).show()
-            }
+        companion object {
+            fun newInstance() = ConversationFragment()
         }
-        stopAndSaveButton.setOnClickListener {
-            if (isClickedRecordAudio) {
-                viewModel.stopAndSaveAudio()
-                isClickedRecordAudio = false
-            } else {
-                Toast.makeText(context, "Please start record", Toast.LENGTH_LONG).show()
-            }
+
+        override fun onCreateView(
+            inflater: LayoutInflater,
+            container: ViewGroup?,
+            savedInstanceState: Bundle?
+        ): View? {
+
+            val factory = InjectorUtils.provideQuotesViewModelFactory(context!!.applicationContext)
+
+            viewModel = ViewModelProviders.of(this, factory)
+                .get(ConversationViewModel::class.java)
+
+            return inflater.inflate(R.layout.conversation_fragment, container, false)
         }
-        addPersonButton.setOnClickListener { viewModel.addPerson() }
-    }
 
-    private fun initRecycleView() {
-        recyclerView.layoutManager = LinearLayoutManager(requireContext())
-        recyclerView.adapter = adapter
+        override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+            super.onViewCreated(view, savedInstanceState)
+            initRecycleView()
+            initListeners()
+            initObservers()
+        }
 
-        recyclerViewPerson.layoutManager =
-            LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL, false)
-        recyclerViewPerson.adapter = adapterPerson
+        private fun initObservers() {
+            viewModel.getAllAudioMessages().observe(viewLifecycleOwner, Observer { audioMessages ->
+                adapter.setAudioMessages(audioMessages!!)
+            })
+
+            viewModel.getAllPersons().observe(viewLifecycleOwner, Observer { persons ->
+                persons?.let { adapterPerson.setPersons(it) }
+            })
+        }
+
+        private fun initListeners() {
+            recordAudioButton.setOnClickListener {
+                if (ContextCompat.checkSelfPermission(
+                        requireActivity(),
+                        Manifest.permission.RECORD_AUDIO
+                    ) != PackageManager.PERMISSION_GRANTED && ContextCompat.checkSelfPermission(
+                        requireActivity(),
+                        Manifest.permission.WRITE_EXTERNAL_STORAGE
+                    ) != PackageManager.PERMISSION_GRANTED
+                ) {
+                    val permissions = arrayOf(
+                        Manifest.permission.RECORD_AUDIO,
+                        Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                        Manifest.permission.READ_EXTERNAL_STORAGE
+                    )
+                    ActivityCompat.requestPermissions(requireActivity(), permissions, 0)
+                } else {
+                    viewModel.initMediaRecorder()
+                    viewModel.startRecording()
+                    isClickedRecordAudio = true
+                    Toast.makeText(context, "start record", Toast.LENGTH_LONG).show()
+                }
+            }
+            stopAndSaveButton.setOnClickListener {
+                if (isClickedRecordAudio) {
+                    viewModel.stopAndSaveAudio()
+                    isClickedRecordAudio = false
+                } else {
+                    Toast.makeText(context, "Please start record", Toast.LENGTH_LONG).show()
+                }
+            }
+            addPersonButton.setOnClickListener { viewModel.addPerson() }
+        }
+
+        private fun initRecycleView() {
+            recyclerView.layoutManager = LinearLayoutManager(requireContext())
+            recyclerView.adapter = adapter
+
+            recyclerViewPerson.layoutManager =
+                LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL, false)
+            recyclerViewPerson.adapter = adapterPerson
+        }
     }
 }
-
-
-
